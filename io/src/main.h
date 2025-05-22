5376cdf50cae7165fb36a0ebb7c1294dbef41ae8
facumedi <facu.medina121@gmail.com>
2025-05-22 21:00:30 +0000

    Corrijo especificacion de tipos en procesar_io

--- io/src/main.h
+++ io/src/main.h
@@ -8,6 +8,6 @@ typedef struct {
 
 
 t_config* iniciar_config(void);
-void procesar_io(int socket_kernel, t_log* logger);
+void procesar_io(int socket_kernel, t_log* logger, int codigo_operacion);
 t_peticion_io* deserializar_peticion_io(void* stream);
 void terminar_programa(t_log*, t_config*, int);
